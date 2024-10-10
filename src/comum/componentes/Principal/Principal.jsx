776ade6256374cb22412f6e46358bba0cd69ea0b
Jardson LDS <jardsonluis2017@hotmail.com>
2024-10-09 22:25:56 -0300

    Configuração react-router-dom e ListaProdutos

--- src/comum/componentes/Principal/Principal.jsx
+++ src/comum/componentes/Principal/Principal.jsx
@@ -1,15 +1,12 @@
-import BotaoCustomizado from "../BotaoCustomizado/BotaoCustomizado";
-import "./Principal.css";
+import BotaoCustomizado from '../BotaoCustomizado/BotaoCustomizado';
+import './Principal.css';
 
-function Principal() {
+function Principal ({titulo, children}) {
   return (
-    <main className="principal_root">
-      Principal
-      <BotaoCustomizado aoClicar={() => alert("clicou no Botão")} >Salvar</BotaoCustomizado>
-      <BotaoCustomizado aoClicar={() => alert("clicou no Botão")} >Salvar</BotaoCustomizado>
-      <BotaoCustomizado aoClicar={() => alert("clicou no Botão")} >Salvar</BotaoCustomizado>
+  <main className="principal_root">
+      <h1>{titulo}</h1>
+     {children}
     </main>
-    
   );
 }
 
